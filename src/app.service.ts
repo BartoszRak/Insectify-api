94f0ac45e36a85cf6c81771abc4aa85483266739
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-22 01:19:46 +0200

    feat: Add .env and Config module.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from './common/Config/config.service'
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly config: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!'
+  }
+
+  getHelloWithEnv(): string {
+    return `Hello World ${this.config.get('test')}`
   }
 }
