2f952ead4a9bf284219bb905629fdb11856f1cc6
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-23 01:38:34 +0200

    feat: Connect firebase with storage.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common'
 import { ConfigService } from './common/Config/config.service'
+import { StorageService } from './common/storage/storage.service'
 
 @Injectable()
 export class AppService {
 
   constructor(
-    @Inject('ConfigService') private readonly config: ConfigService
+    @Inject('ConfigService') private readonly config: ConfigService,
+    @Inject('StorageService') private storage: StorageService,
     ) {}
 
   getHello(): string {
+    console.log(this.storage.isStorageService())
     return 'Hello World!'
   }
 
---
+++ src/common/storage/storage.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+
+import { FirebaseModule } from '../firebase/firebase.module'
+import { StorageService } from './storage.service'
+
+@Module({
+  imports: [FirebaseModule],
+  controllers: [],
+  providers: [StorageService],
+  exports: [StorageService],
+})
+export class StorageModule {}
