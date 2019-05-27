e66bbb01a39f2e5ab9d69fc0f005ae52ef7764ab
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-27 02:09:52 +0200

    feat: Isolate interfaces to /interfaces subdir

---
+++ src/services/mailer/interfaces/mailerConfiguration.interface.ts
@@ -0,0 +1,16 @@
+interface MailerAuth {
+  user: string
+  pass: string
+}
+
+
+export interface MailerConfiguration {
+  host: string
+  port: number
+  secure: boolean
+  service: string
+  auth: MailerAuth
+  tls: {
+    rejectUnauthorized: boolean
+  }
+}
