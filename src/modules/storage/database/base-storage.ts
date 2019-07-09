cc7cec9f5da7dfcb948e65cbd10a4f7ae1c50595
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-07-09 17:17:49 +0200

    refactor: Remove mongodb.

--- src/modules/storage/database/base-storage.ts
+++ src/modules/storage/database/base-storage.ts
@@ -1,28 +1,20 @@
-import { DatabaseClient } from './database-client'
 import { FirestoreClient } from './firestore-client'
 
 export abstract class BaseStorage {
 
-  protected db: any
-  protected dbClient: DatabaseClient
   protected fs: any
   protected fsClient: FirestoreClient
 
   constructor() {
-    this.dbClient = new DatabaseClient()
     this.fsClient = new FirestoreClient()
   }
 
   public async connect() {
-    this.db = await this.dbClient.connect()
     this.fs = await this.fsClient.connect()
     console.log('[Base Storage] Database client connected.')
   }
 
   public async close() {
-    if (this.dbClient && this.dbClient.connected) {
-      await this.dbClient.close()
-      console.log('[Base Storage] Database client disconnected.')
-    }
+    console.log('[Base Storage] Database client disconnected.')
   }
 }
