7f7fec62075e429861883fe588206c4c08bc19d0
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-07-09 03:11:25 +0200

    feat: Add permissions checking.

--- src/guards/permissions/permissions.guard.ts
+++ src/guards/permissions/permissions.guard.ts
@@ -1,8 +1,9 @@
-import { Injectable, Inject, CanActivate, ExecutionContext } from '@nestjs/common'
+import { Injectable, Inject, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { GqlExecutionContext, GraphQLExecutionContext } from '@nestjs/graphql'
 import { Express } from 'express'
 import * as jwt from 'jsonwebtoken'
+import * as memoize from 'memoizee'
 
 import { jwtSecret } from '../../config'
 import { User } from '../../graphql.schema'
@@ -14,28 +15,96 @@ export class PermissionsGuard implements CanActivate {
   constructor(
     @Inject('StorageService') private readonly storage: StorageService,
     private readonly reflector: Reflector,
-  ) {}
+  ) {
+    this.init()
+  }
 
-  protected initialized: boolean = false
-  protected roles: Role[]
+  private roles: Role[]
 
-  protected init() {
+  private async init() {
     try {
       this.storage.roles.onChange((data: any) => {
         this.roles = data
       })
-      this.initialized = true
+      console.log(`[Permissions Guard] Initialized.`)
     } catch (err) {
-      console.log(`[Permissions Guard] Init error: ${err.message}`)
+      console.log(`[Permissions Guard] Init error: ${err.message}.`)
+      await new Promise((resolve: any, reject: any) => {
+        try {
+          setTimeout(() => {
+            this.init()
+            resolve()
+          }, 1000)
+        } catch(err) {
+          reject()
+        }
+      })
     }
+  }
+
+  private createInfinityProxy() {
+    return new Proxy({}, {
+      get: () =>this.createInfinityProxy()
+    })
+  }
+
+  private mapPermissionsArrayToObject(userPermissionsArray: string[]): any {
+    return memoize((userPermissions: string[]) => {
+      return userPermissions.reduce((userPermissionsObject: any, permissionsString: string): any => {
+        const permissionsArray: string[] = permissionsString.split('.')
+        let ref: any = userPermissionsObject
+        permissionsArray.forEach((permission: string, index: number) => {
+          const nextPermOrActual: any = permissionsArray.length <= 1 ? permission : permissionsArray[index + 1]
+          const baseObj: any = ref[permission] || {}
+          const proxy: any =  new Proxy(baseObj, {
+            get: (target, name) => target['*'] ? this.createInfinityProxy() : target[name]
+          })
+          const readyObj: any = nextPermOrActual === '*' ? proxy : baseObj
+  
+          ref[permission] = Boolean(permission === '*') || readyObj
+          ref = ref[permission]
+        })
+        return userPermissionsObject
+      }, {})
+    })(userPermissionsArray)
+  }
+
+
+  private getUserPermissions(user: User) {
+    return this.roles.reduce((permissionsArray: string[], role: Role) => {
+      return user.roles[role.name]
+      ? [ ...permissionsArray, ...Object.keys(role.permissions).filter(key => Boolean(role.permissions[key])).map(key => key)]
+      : permissionsArray
+    }, [])
+  }
+  
+
+  private hasAccess(requiredPermissionsArray: string[], userPermissionsObject: any): boolean {
+    return memoize((requiredPermissions: string[], userPermissionsObj: any): boolean => {
+      const requiredPermissionsArray: string[][] = requiredPermissions.map((perm: string): string[] => perm.split('.'))
+      let accessFlag: boolean = true
+  
+      requiredPermissionsArray.some((requiredPermission: string[]): boolean => {
+        if (!accessFlag) return true
+  
+        let ref: any = userPermissionsObj
+        requiredPermission.some((permission: string): boolean => {
+          if (ref['*']) return true
+          if (!ref[permission]) {
+            accessFlag = false
+            return true
+          }
+          ref = ref[permission]
+        })
+      })
+  
+      return accessFlag
+    })(requiredPermissionsArray, userPermissionsObject)
   }
 
   public async canActivate(
     ctx: ExecutionContext,
   ): Promise<boolean> {
-    if (!this.initialized) {
-      this.init()
-    }
     const graphqlCtx: GraphQLExecutionContext = GqlExecutionContext.create(ctx)
     const request: any = ctx.switchToHttp().getRequest() || graphqlCtx.getContext()
     const requiredPermissions: string[] = this.reflector.get<string[]>('requiredPermissions', ctx.getHandler())
@@ -49,8 +118,13 @@ export class PermissionsGuard implements CanActivate {
     const token: string = header.authorization.slice(7)
     const decodedToken: any = await jwt.verify(token, jwtSecret)
     const user: User = decodedToken.data.user
-    console.log(user)
-    console.log(requiredPermissions)
+    const userPermissions: string[] = this.getUserPermissions(user)
+    const userPermissionsObj: any = this.mapPermissionsArrayToObject(userPermissions)
+  
+    if (!this.hasAccess(requiredPermissions, userPermissionsObj)) {
+      throw new UnauthorizedException('You are missing some permissions to access this resource.')
+    }
+
     return true
   }
 }
