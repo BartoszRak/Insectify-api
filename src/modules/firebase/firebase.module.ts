2a8a627cb9ff1979c838e4c4ee44d4ef361ed8a9
Bartosz Rak <49698017+BartoszRak@users.noreply.github.com>
2019-07-09 17:07:03 +0200

    feat: Update credentials and remove modules.

--- src/modules/firebase/firebase.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-
-import { firebaseProviders } from './firebase.provider'
-
-@Module({
-  providers: [...firebaseProviders],
-  exports: [...firebaseProviders],
-})
-export class FirebaseModule {}
